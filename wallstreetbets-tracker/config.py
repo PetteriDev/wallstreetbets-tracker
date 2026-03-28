253bfee100d24b86f059234587b23e171ebe47ff
PetteriDev <91135131+PetteriDev@users.noreply.github.com>
2026-03-28 21:37:05 +0200

    Add files via upload

---
+++ wallstreetbets-tracker/config.py
@@ -0,0 +1,10 @@
+DB_HOST = 'localhost'
+DB_USER = 'postgres'
+DB_PASS = 'postgres'
+DB_NAME = 'postgres'
+
+API_URL = 'https://paper-api.alpaca.markets'
+API_KEY = 'PKFXHP4WHQ45ZM0HZ568'
+API_SECRET = 'VDT0abbrVSKj13QHhEzWK6O2azIdAIL4ps2HWd6i'
+
+
